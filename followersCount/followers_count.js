5fac38473522092805e6db4d399abb140ef25e10
Usha Rani <usharanig92@gmail.com>
2024-11-23 18:54:27 -0500

    message

---
+++ followersCount/followers_count.js
@@ -0,0 +1,15 @@
+let count = 0
+
+function increaseCount(){
+    count++
+    document.getElementById('countDisplay').innerText = count
+    checkCountValue()
+}
+
+function checkCountValue(){
+    if (count === 10) {
+        alert("your instagram post reached 10 followers")
+    } else if (count === 20) {
+        alert("your instagram post reached 20 followers")
+    }
+}
