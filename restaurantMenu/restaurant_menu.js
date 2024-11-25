04fe9d6474a640a1ef4c2cb7ca60aa1a3885d0d1
Usha Rani <usharanig92@gmail.com>
2024-11-24 19:46:24 -0500

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const mainCourseMenu = ['Steak -$20', 'Pasta -$18', 'Burger -$15', 'Salmon -$12'];
+const dessertMenu = ['Cake - $9', 'Ice Cream -$3', 'Pudding -$7', 'Fruit Salad -$5'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item,index)=> `<p>Item ${index+1}: ${item}</p>`).join('')
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML
